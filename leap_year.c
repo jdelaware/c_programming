50a73cdc6647c3a88c3401dd9a5655faedbb7610
jdelaware <jonah.delaware@gmail.com>
2022-02-22 13:57:03 +0100

    Add important condition

--- leap_year.c
+++ leap_year.c
@@ -4,9 +4,9 @@ int main() {
   int anno = 0;
 
   printf("Wanna know which year is a leap year? ");
-  scanf("%i\n", &anno);
+  scanf("%i", &anno);
 
-  anno % 4 == 0 && anno % 100 !=0 ? printf("\n%i is a leap year.\n", anno) : printf("\n%i is not a leap year.\n", anno);
+  (anno % 4 == 0 && anno % 100 !=0) || anno % 400 == 0 ? printf("\n%i is a leap year.\n", anno) : printf("\n%i is not a leap year.\n", anno);
 
   return 0;
 }
